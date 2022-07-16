54abcb7caf13d64616dcf9b854b23519bd003157
Cosimo Chellini <cosimo.chellini@gmail.com>
2022-07-16 20:04:25 +0200

    fix(ts): small ts error

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
 import { signOut } from "next-auth/react";
 import { useAuth } from "../src/hooks/auth.hook";
-import { fireOnce } from "../src/hooks";
+import { useEffectOnce } from "../src/hooks";
 
 export default function Logout() {
   const router = useRouter();
   const { onUnauthenticated } = useAuth();
 
-  fireOnce(() => {
+  useEffectOnce(() => {
     signOut({}).then(() => router.push("/"));
 
     onUnauthenticated();
