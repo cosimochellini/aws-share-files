074ff8ac6ca856c403e6185e064db2777da57d3c
Cosimo Chellini <cosimo.chellini@gmail.com>
2022-11-27 11:33:41 +0100

    fix(dep): updated dependecy

--- next.config.js
+++ next.config.js
@@ -13,6 +13,21 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: withWebpack,
   redirects: withRedirects,
+  optimization: {
+    mergeDuplicateChunks: true,
+  },
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-env': {
+          targets: {
+            browsers: '>1%, not ie 11, not op_mini all',
+          },
+        },
+      },
+    ],
+  ],
 };
 
 module.exports = withPWA(nextConfig);
