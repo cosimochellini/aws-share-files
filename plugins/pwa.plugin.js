d36cb635e267fd66bc7dbdbcc01196bf26fc74b7
Cosimo Chellini <cosimo.chellini@gmail.com>
2023-04-09 14:10:02 +0200

    feat(cache): added more performante cache

--- plugins/pwa.plugin.js
+++ plugins/pwa.plugin.js
@@ -1,7 +1,22 @@
-const runtimeCaching = require('next-pwa/cache');
 const withPWA = require('next-pwa')({
   dest: 'public',
-  runtimeCaching,
+  runtimeCaching: [
+    {
+      // Cache all static files
+      urlPattern: /^https?.*\.(png|jpg|jpeg|gif|webp|svg|ico|css|js|woff|woff2|json)/,
+      // Apply stale-while-revalidate strategy
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-cache',
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+        cacheableResponse: {
+          statuses: [200],
+        },
+      },
+    },
+  ],
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
