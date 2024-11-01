290e700ee87679077f6a4a0f40d238ef6b7a1d38
Cosimo Chellini <cosimo.chellini@gmail.com>
2024-11-01 17:09:36 +0100

    Revert "fix(dep): updated dependecy"
    This reverts commit 5e878b60657964460052e1aa19be4cd322bf9664.

--- script.runner.node.ts
+++ script.runner.node.ts
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
 
 const scriptDirectory = './scripts/';
 
@@ -8,14 +7,11 @@ const run = async () => {
 
   console.log('files:', files);
 
-  for (const file of files) {
-    const modulePath = path.join(scriptDirectory, file);
-    // eslint-disable-next-line no-await-in-loop
-    const { default: script } = await import(modulePath);
+  for await (const file of files) {
+    const script = require(`${scriptDirectory}${file}`).default;
 
     console.log(`Running ${file}`);
 
-    // eslint-disable-next-line no-await-in-loop
     await script();
   }
 };
